a9afce97ca0af0ae1b6ef34a0c9645cd9c5a3141
Gaurav Suhag <gauravsuhag7ds@gmail.com>
2021-04-03 15:44:25 +0530

    add to github

---
+++ client/src/components/pages/About.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <div>
+      <h1>About This App</h1>
+      <p className='my-1'>
+        Hello! my name is Gaurav Suhag.<br/>I made this website when I was doing a course on Full Stack Web Development.<br/>The front end of this website is made using React and modern React features like React Hooks and the Context Api.<br/>The backend server of this website is made using Node.js and Express Framework. The database used is mongoDB.
+      </p>
+    </div>
+  );
+};
+
+export default About;
